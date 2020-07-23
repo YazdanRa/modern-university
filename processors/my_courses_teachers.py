638108633cc9578f76c0705cdf95d0049102d083
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-07-23 14:40:32 +0430

    - update my courses teachers

--- processors/my_courses_teachers.py
+++ processors/my_courses_teachers.py
@@ -1,2 +1,12 @@
-def setup(student):
-    return None
+from texttable import Texttable
+
+from models import Course
+
+
+def setup(teacher):
+    table = Texttable().add_rows([
+        ['ID', 'Course title', 'Is_ Active']
+    ])
+    for course in Course.select().where(Course.teacher == teacher):
+        table.add_row([course.id, course.title, course.is_active])
+    print(table.draw())
