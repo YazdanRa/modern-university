4029b7d0020e69b63111208a297ab7abcc80a1f7
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-07-23 15:33:49 +0430

    - update my results

--- processors/my_results.py
+++ processors/my_results.py
@@ -1,2 +1,21 @@
+from texttable import Texttable
+
+from models import Mark, Course
+
+
+def get_course_name(course):
+    course = Course.get(Course.id == course)
+    return course.title
+
+
+def result_mark(mark):
+    return str(mark) if mark else 'Not recorded!'
+
+
 def setup(student):
-    return None
+    table = Texttable().add_rows([
+        ['Course title', 'Your mark']
+    ])
+    for mark in Mark.select().where(Mark.student == student):
+        table.add_row([get_course_name(mark.course), result_mark(mark.mark)])
+    print(table.draw())
