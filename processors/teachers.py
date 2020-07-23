d764c45f04b5d7251c39d367b059a091f2c3ad23
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-07-23 14:25:36 +0430

    - update teachers

--- processors/teachers.py
+++ processors/teachers.py
@@ -1,2 +1,12 @@
+from texttable import Texttable
+
+from models import User
+
+
 def setup():
-    return None
+    table = Texttable().add_rows([
+        ['ID', 'First name', 'Last name',  'Teacher number']
+    ])
+    for teacher in User.select().where(User.is_teacher == True):
+        table.add_row([teacher.id, teacher.first_name, teacher.last_name, teacher.student_number])
+    print(table.draw())
