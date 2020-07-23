c12f3fdcdabeab0206751d444a6143ba58b480c3
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-07-23 15:09:29 +0430

    - update edit info

--- processors/edit_info.py
+++ processors/edit_info.py
@@ -1,5 +1,7 @@
 def setup(user):
     user.first_name = input('Enter your first name: ')
     user.last_name = input('Enter your last name: ')
+    user.hometown = input('Enter your hometown: ')
+    user.national_id = input('Enter your National ID: ')
     user.save()
     print('your information successfully updated!')
