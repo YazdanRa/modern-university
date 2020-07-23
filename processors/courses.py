cc25319a6e067cd55dc2a90bb1b764ef8437fb7e
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-07-23 14:21:54 +0430

    - update courses

--- processors/courses.py
+++ processors/courses.py
@@ -1,2 +1,17 @@
+from texttable import Texttable
+
+from models import Course, User
+
+
+def get_teacher(course):
+    teacher = User.get(User.id == course.teacher)
+    return '{} {}'.format(teacher.first_name, teacher.last_name)
+
+
 def setup():
-    return None
+    table = Texttable().add_rows([
+        ['ID', 'Title', 'Teacher', 'Is_Active']
+    ])
+    for course in Course.select():
+        table.add_row([course.id, course.title, get_teacher(course), course.is_active])
+    print(table.draw())
