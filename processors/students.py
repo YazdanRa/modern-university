39307b0747559eec8afe26132501cde46d37b6b1
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-07-23 14:26:39 +0430

    - update students

--- processors/students.py
+++ processors/students.py
@@ -1,2 +1,12 @@
+from texttable import Texttable
+
+from models import User
+
+
 def setup():
-    return None
+    table = Texttable().add_rows([
+        ['ID', 'First name', 'Last name',  'Student number']
+    ])
+    for student in User.select().where(User.is_student == True):
+        table.add_row([student.id, student.first_name, student.last_name, student.student_number])
+    print(table.draw())
