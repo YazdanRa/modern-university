a2c66258b1474125abf0adfff7584b4b72fd6984
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-07-30 01:20:07 +0430

    - fix teachers display

--- processors/teachers.py
+++ processors/teachers.py
@@ -3,14 +3,17 @@ import curses
 from texttable import Texttable
 
 from models import User
-from render import draw_menu
+from render import draw_menu, show_message
 
 
 def setup():
-    table = Texttable().add_rows([
-        ['ID', 'First name', 'Last name',  'Teacher number']
-    ])
-    for teacher in User.select().where(User.is_teacher == True):
-        table.add_row([teacher.id, teacher.first_name, teacher.last_name, teacher.student_number])
-    while curses.wrapper(draw_menu, table.draw()) != ord('b'):
+    teachers = User.select().where(User.is_teacher == True)
+    if not len(teachers):
+        curses.wrapper(show_message, 'list is empty!')
+        return
+    t = [['ID', 'First name', 'Last name',  'Teacher number'], ]
+    for teacher in teachers:
+        t.append([teacher.id, teacher.first_name, teacher.last_name, teacher.student_number])
+    table = Texttable().add_rows(t).draw()
+    while curses.wrapper(draw_menu, table) != ord('b'):
         pass
