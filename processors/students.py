5c4c38cda56e63ac60917c5a85c6b78ef6ccd7f3
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-07-30 01:26:03 +0430

    - fix students empty display

--- processors/students.py
+++ processors/students.py
@@ -3,14 +3,17 @@ import curses
 from texttable import Texttable
 
 from models import User
-from render import draw_menu
+from render import draw_menu, show_message
 
 
 def setup():
-    table = Texttable().add_rows([
-        ['ID', 'First name', 'Last name',  'Student number']
-    ])
-    for student in User.select().where(User.is_student == True):
-        table.add_row([student.id, student.first_name, student.last_name, student.student_number])
-    while curses.wrapper(draw_menu, table.draw()) != ord('b'):
+    students = User.select().where(User.is_student == True)
+    if not len(students):
+        curses.wrapper(show_message, 'list is empty!')
+        return
+    t = [['ID', 'First name', 'Last name',  'Student number'], ]
+    for student in students:
+        t.append([student.id, student.first_name, student.last_name, student.student_number])
+    table = Texttable().add_rows(t).draw()
+    while curses.wrapper(draw_menu, table) != ord('b'):
         pass
